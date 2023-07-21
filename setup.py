1f3efa96ac80896908d8e5d3600c2d43488aff35
Emre Dayangac <120194760+EmreDay1@users.noreply.github.com>
2023-07-21 03:29:53 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import subprocess
+import os
+
+os.system("echo hello")
+os.system('sudo apt-get install libcblas-dev')
+os.system('sudo apt-get install libhdf5-dev')
+os.system('sudo apt-get install libhdf5-serial-dev')
+os.system('sudo apt-get install libqtwebkit4')
+os.system('sudo apt-get install libqt4-test')
+os.system('sudo apt-get install libatlas-base-dev')
+os.system('sudo apt-get install libjasper-dev')
+
+setup(
+    name = "Pi20",
+    version = "0.1.5",
+    author = " Emre Dayangac, Poyraz Ozzengi",
+    author_email = "emre.dayangac@hisarschool.k12.tr, poyraz.ozzengi@hisarschool.k12.tr",
+    description = "Library that makes use of sensors, motors, and servos in the PiWars Turkey robot kit by HisarCS",
+    packages = ["Pi20"],
+    classifiers=["Development Status :: 1 - Alpha"],
+    install_requires=[
+        'picamera',
+        'pygame',
+        'RPi.GPIO',
+        'wiringpi',
+        'numpy',
+        'opencv-contrib-python==3.4.3.18'
+    ]
+
+)
