2d5e9b1437e9157df5a7b9ff8ae93fb43b52521c
Emre Dayangac <120194760+EmreDay1@users.noreply.github.com>
2023-07-22 01:34:51 +0300

    Update __init__.py

--- Piwars-2023-Library/__init__.py
+++ Piwars-2023-Library/__init__.py
@@ -1,5 +1,5 @@
 from .Kumanda import Kumanda
-from .motor import Motor
+from .Motor import Motor
 from .servo import Servo
 from .ultrasonik import Ultrasonik_S
 from .kamera import HizlandirilmisPiKamera
